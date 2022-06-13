acd5540d64ab077ed9f118bd5230254ce53aada0
Syed Saim <syedsaim40@gmail.com>
2022-06-13 23:35:37 +0500

    first commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React from step by step
         </a>
       </header>
     </div>
