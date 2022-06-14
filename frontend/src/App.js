94e598b48c1320c9fef935b189bf3f4869228fe6
Syed Saim <syedsaim40@gmail.com>
2022-06-14 13:24:06 +0500

    changes frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,21 @@
 import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+import Header from './Components/Layout/Header/Header';
+import Home from './Components/Home/Home';
+import About from './Components/Pages/About';
 
 function App() {
   return (
-    <div className="App">
-  <p>Hello World</p>
+    <div>
+      <Router>
+        <Header />
+        <Routes>
+          <Route exact path='/' component={Home}></Route>
+          <Route exact path='/about' component={About}></Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
