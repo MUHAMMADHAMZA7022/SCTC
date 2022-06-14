30c8c599e63e76dfedd27a2ef5cdad6f1a280a89
Syed Saim <syedsaim40@gmail.com>
2022-06-14 14:43:08 +0500

    cgg

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,22 @@
 import './App.css';
-import React from 'react';
+import { useEffect } from "react";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import WebFont from "webfontloader";
 
 import Header from './Components/Layout/Header/Header';
 import Home from './Components/Home/Home';
 import About from './Components/Pages/About';
+import SignUp from './Components/LoginPage/SignUp';
 
 function App() {
+
+  useEffect(() => {
+    WebFont.load({
+      google: {
+        families: ["Noto Sans", "sans-serif"],
+      },
+    });
+  }, []);
   return (
     <div>
       <Router>
@@ -14,6 +24,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Home/>}/>
           <Route path='/about' element={<About/>}/>
+          <Route path='/signup' element={<SignUp/>}/>
         </Routes>
       </Router>
     </div>
--- frontend/src/Components/LoginPage/SignUp.js
+++ frontend/src/Components/LoginPage/SignUp.js
@@ -1,9 +1,53 @@
-import React from 'react'
+import React, { Fragment } from 'react'
+import "./SignUp.css";
 
 function SignUp() {
-  return (
-    <div>SignUp</div>
-  )
+    return (
+        <Fragment>
+            {/* Sign Up Form */}
+            <div className='formHolder'>
+                <div className='signupHolder'>
+                    <h1>Sign Up</h1>
+                    <form className="signupForm">
+                        {/* Name */}
+                        <div className="group_field">
+                            <label htmlFor="name">
+                                Name<span>*</span>
+                            </label>
+                            <input
+                                type='text'
+                                placeholder="Name"
+                                required
+                            />
+                        </div>
+                        {/* Email */}
+                        <div className="group_field">
+                            <label htmlFor="email">
+                                Email<span>*</span>
+                            </label>
+                            <input
+                                type='email'
+                                placeholder="Email"
+                                required
+                            />
+                        </div>
+                        {/* Password */}
+                        <div className="group_field">
+                            <label htmlFor="password">
+                            Password<span>*</span>
+                            </label>
+                            <input
+                                type='password'
+                                placeholder="Password"
+                                required
+                            />
+                        </div>
+                    </form>
+                </div>
+                <div className='signUpImg'></div>
+            </div>
+        </Fragment>
+    )
 }
 
 export default SignUp
