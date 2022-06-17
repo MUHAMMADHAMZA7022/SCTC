930e174b6c1389f2a2fecf1718a571b03096dd22
Syed Saim <syedsaim40@gmail.com>
2022-06-17 20:08:16 +0500

    Merge branch 'master' of https://github.com/MUHAMMADHAMZA7022/SCTC

--- frontend/src/Components/Layout/Header/Header.js
+++ frontend/src/Components/Layout/Header/Header.js
@@ -1,8 +1,26 @@
-import React from 'react'
+import React, { Fragment } from 'react';
+import './Header.css';
+import {Link} from 'react-router-dom';
 
 function Header() {
   return (
-    <div>Header</div>
+    <Fragment>
+      <div className='header_holder'>
+        <div className='logo_holder'>
+          <Link to='/'>SCTC LOGO</Link>
+        </div>
+        <div className='navbar'>
+          <ul className='unstyled nav_list'>
+            <li><Link to='/'>Home</Link></li>
+            <li><Link to='/courses'>Courses</Link></li>
+            <li><Link to='/about'>About</Link></li>
+            <li><Link to='/events'>Events</Link></li>
+            <li><Link to='/contact'>Contact</Link></li>
+            <li><Link to='/login' className='nav_btn'>Sign In</Link></li>
+          </ul>
+        </div>
+      </div>      
+    </Fragment>
   )
 }
 
