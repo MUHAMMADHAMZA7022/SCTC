4b9ec1776e2c7faef79aba16f6df98875e062a28
Syed Saim <syedsaim40@gmail.com>
2022-06-19 15:50:56 +0500

    changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -59,10 +59,6 @@ const { user, isAuthenticated } = useSelector((state) => state.user);
           <Route path='/signup' element={<SignUp />} />
           <Route path='/login' element={<Login />} />         
           <Route path='/forget' element={<ForgetPassword />} />
-<<<<<<< HEAD
-          <Route path='/reset' element={<ResetPassword />} />
-        
-=======
           <Route path='/reset/:token' element={<ResetPassword />} />
           <Route path='/courseorder' element={<Courseorder/>} />
           <Route path='/updatecourse' element={<UpdateCourse />} />
@@ -73,7 +69,6 @@ const { user, isAuthenticated } = useSelector((state) => state.user);
           <Route path='/students' element={<Students />} />
           <Route path='/seminars' element={<Seminars />} />
         {/* ADMIN ROUTES */}
->>>>>>> ba637078fbb19470c95b38310029d66cc46e08e5
           <Route path='/dashboard' element={
           <ProtectedRoute isAuthenticated={isAuthenticated}  adminRoute={true} isAdmin={user &&user.role==="admin"?true:false}>
               <Dashboard />
@@ -86,18 +81,8 @@ const { user, isAuthenticated } = useSelector((state) => state.user);
           <ProtectedRoute isAuthenticated={isAuthenticated}  adminRoute={true} isAdmin={user &&user.role==="admin"?true:false}>
              <Allcourses />
             </ProtectedRoute>} />
-<<<<<<< HEAD
-          <Route path='/courseorder' element={<Courseorder/>} />
-          <Route path='/updatecourse' element={<UpdateCourse />} />
-          <Route path='/removecourse' element={<RemoveCourses />} />
-       
-          
-          <Route path='/all/events' element={<Events />} />
-          <Route path='/create/event' element={<CreateEvent />} />
-=======
         
         {/* NORMAL ROUTES */}
->>>>>>> ba637078fbb19470c95b38310029d66cc46e08e5
           <Route path='/profile' element={
           <ProtectedRoute isAuthenticated={isAuthenticated}>
               <Profile />
@@ -108,13 +93,6 @@ const { user, isAuthenticated } = useSelector((state) => state.user);
             </ProtectedRoute>} />
 
           
-<<<<<<< HEAD
-          <Route path='/sbr' element={<Sidebar />} />
-          <Route path='/students' element={<Students />} />
-          <Route path='/seminars' element={<Seminars />} />
-          <Route path='/profile' element={<Profile />} />
-=======
->>>>>>> ba637078fbb19470c95b38310029d66cc46e08e5
         </Routes>
       </Router>
     </div>
