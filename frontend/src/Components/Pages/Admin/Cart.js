5759ffbb1856c62c86b37f8cbd6372fded67f3b9
Syed Saim <syedsaim40@gmail.com>
2022-06-23 23:45:50 +0500

    changes

--- frontend/src/Components/Pages/Admin/Cart.js
+++ frontend/src/Components/Pages/Admin/Cart.js
@@ -7,9 +7,39 @@ import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
 function Cart() {
     return (
         <Fragment>
-            <div className='Cart'>
+            <div className='cart'>
+                <h1>Shopping Cart</h1>
                 <div className='cart_holder'>
-                    <h1>Shopping Cart</h1>
+                    <div className='cart_courses'>
+                        <div className='cart_crs_img'>
+                            <img src={bg3} alt="course-img" />
+                        </div>
+                        <div className='cart_crs'>
+                            <div className='cart_crs_content'>
+                                <h2 className='cart_crs_title'>Course Title</h2>
+                                <span className='cart_remove_btn'><DeleteForeverIcon /></span>
+                                <span className='cart_crs_price'>
+                                    <span>$</span>
+                                    55
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='cart_courses'>
+                        <div className='cart_crs_img'>
+                            <img src={bg3} alt="course-img" />
+                        </div>
+                        <div className='cart_crs'>
+                            <div className='cart_crs_content'>
+                                <h2 className='cart_crs_title'>Course Title</h2>
+                                <span className='cart_remove_btn'><DeleteForeverIcon /></span>
+                                <span className='cart_crs_price'>
+                                    <span>$</span>
+                                    55
+                                </span>
+                            </div>
+                        </div>
+                    </div>
                     <div className='cart_courses'>
                         <div className='cart_crs_img'>
                             <img src={bg3} alt="course-img" />
