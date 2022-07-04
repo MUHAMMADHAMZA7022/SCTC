7bc25eef64c0af918a1b794d16b6a58a29027b35
Syed Saim <syedsaim40@gmail.com>
2022-07-04 23:15:38 +0500

    changes

--- frontend/src/Components/Layout/Loader/cloader.js
+++ frontend/src/Components/Layout/Loader/cloader.js
@@ -4,31 +4,33 @@ const cloader = () => {
   return (
     <div>
       <div className="grid">
-        <div className="crsCard_loader">
-          <div className="crsImg_loader"></div>
-          <div className="crsCardLoader_content">
-            <div className="windows8">
-              <div className="wBall" id="wBall_1">
-                <div className="wInnerBall"></div>
-              </div>
-              <div className="wBall" id="wBall_2">
-                <div className="wInnerBall"></div>
-              </div>
-              <div className="wBall" id="wBall_3">
-                <div className="wInnerBall"></div>
-              </div>
-              <div className="wBall" id="wBall_4">
-                <div className="wInnerBall"></div>
-              </div>
-              <div className="wBall" id="wBall_5">
-                <div className="wInnerBall"></div>
+        <div className="crsLoader_card">
+          <div className="crsCard_loader">
+            <div className="crsImg_loader"></div>
+            <div className="crsCardLoader_content">
+              <div className="windows8">
+                <div className="wBall" id="wBall_1">
+                  <div className="wInnerBall"></div>
+                </div>
+                <div className="wBall" id="wBall_2">
+                  <div className="wInnerBall"></div>
+                </div>
+                <div className="wBall" id="wBall_3">
+                  <div className="wInnerBall"></div>
+                </div>
+                <div className="wBall" id="wBall_4">
+                  <div className="wInnerBall"></div>
+                </div>
+                <div className="wBall" id="wBall_5">
+                  <div className="wInnerBall"></div>
+                </div>
               </div>
             </div>
           </div>
         </div>
-        
+
       </div>
-      
+
     </div>
   );
 };
