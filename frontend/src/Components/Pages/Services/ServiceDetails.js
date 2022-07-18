9279e5663dcdc0db387db885f9ceb8ae564d5b14
Syed Saim <syedsaim40@gmail.com>
2022-07-18 18:51:52 +0500

    changes

--- frontend/src/Components/Pages/Services/ServiceDetails.js
+++ frontend/src/Components/Pages/Services/ServiceDetails.js
@@ -1,5 +1,5 @@
 import bg3 from '../../../images/slide-img-3.jpg';
-import React, { Fragment, useState,useEffect  } from "react";
+import React, { Fragment, useState, useEffect } from "react";
 import { Link } from 'react-router-dom';
 import TurnedInIcon from '@mui/icons-material/TurnedIn';
 import { useSelector, useDispatch } from "react-redux";
@@ -34,44 +34,47 @@ function ServiceDetails() {
 
 
 
-  return (
-    <Fragment>
-        <div className='eventsDetails'>
-            {/* Single Page Banner */}
-            <div className='evn_banner'>
-                <div className='evn_bannerImg'>
-                    <img src={bg3} alt="banner" />
+    return (
+        <Fragment>
+            <div className='eventsDetails'>
+                {/* Single Page Banner */}
+                <div className='evn_banner'>
+                    <div className='evn_bannerImg'>
+                        <img src={bg3} alt="banner" />
+                    </div>
+                    <div className="evnBanner_content">
+                        <h2>Service Detailed</h2>
+                    </div>
                 </div>
-                <div className="evnBanner_content">
-                    <h2>Service Detailed</h2>
-                </div>
-            </div>
-            {/* Single Page Course Details */}
-            <div className='evnDetailsContent grid'>
-                <div className='evnDetail_title'>
-                    <h2>{service&&service.name}</h2>
-                </div>
-                <div className='evnDetail_desp'>
-                    <p>{service&&service.description}</p>
-                </div>
-                <div className='evnDetail_action'>
-                    <Link to="#" onClick={ToggleClass}><TurnedInIcon />Take Appointment</Link>
-                    <div className={isActive ? "evn_box" : null}>
-                        <div className='eventJoin_holder'>
-                            <p className='capitalize'>for contact to consultant please fill the below information*</p>
-                            <form className='eventJoin_form'>
-                                <input type={"text"} placeholder="Name" />
-                                <input type={"email"} placeholder="Email" />
-                                <textarea placeholder='Your message for consultant'></textarea>
-                                <button className='btn_primary'>Send Message</button>
-                            </form>
+                {/* Single Page Course Details */}
+                <div className='evnDetailsContent grid'>
+                    <div className='event_img'>
+                        <img src={service?.images?.url} alt="courseImg" />
+                    </div>
+                    <div className='evnDetail_title'>
+                        <h2>{service && service.name}</h2>
+                    </div>
+                    <div className='evnDetail_desp'>
+                        <p>{service && service.description}</p>
+                    </div>
+                    <div className='evnDetail_action'>
+                        <Link to="#" onClick={ToggleClass}><TurnedInIcon />Take Appointment</Link>
+                        <div className={isActive ? "evn_box" : null}>
+                            <div className='eventJoin_holder'>
+                                <p className='capitalize'>for contact to consultant please fill the below information*</p>
+                                <form className='eventJoin_form'>
+                                    <input type={"text"} placeholder="Name" />
+                                    <input type={"email"} placeholder="Email" />
+                                    <textarea placeholder='Your message for consultant'></textarea>
+                                    <button className='btn_primary'>Send Message</button>
+                                </form>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-    </Fragment>
-  )
+        </Fragment>
+    )
 }
 
 export default ServiceDetails
