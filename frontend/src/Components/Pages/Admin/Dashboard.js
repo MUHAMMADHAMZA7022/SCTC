605315db0ed17f95de9c4f6702ee7ddc329b4a01
Syed Saim <syedsaim40@gmail.com>
2022-07-19 19:15:20 +0500

    changes

--- frontend/src/Components/Pages/Admin/Dashboard.js
+++ frontend/src/Components/Pages/Admin/Dashboard.js
@@ -1,4 +1,5 @@
 import React, { Fragment } from 'react'
+import { Link } from 'react-router-dom';
 import "./Dashboard.css";
 import Sidebar from './Sidebar';
 
@@ -13,7 +14,11 @@ function Dashboard() {
                 {/* Main Content */}
                 <div className='dashboard_content'>
                     <div className='dashboardContent_holder'>
-                        jbfhdbfgv
+                        <div className='dasCard'>
+                            <h3>Total No of Courses</h3>
+                            <span>174</span>
+                            <Link to={"courses"}>See all courses</Link>
+                        </div>
                     </div>
                 </div>
             </div>
